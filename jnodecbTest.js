3e84e6e4d85b989ceff6cdc614608379c38d5737
Christopher J. Brody <brodybits@litehelpers.net>
2015-09-21 18:20:08 +0200

    Fix jnodecbTest.js lint issue

--- jnodecbTest.js
+++ jnodecbTest.js
@@ -1,10 +1,10 @@
 var JNodeCB = require('./build/Release/JNodeCB.node');
 
-var testMethodResult = JNodeCB.callMethod("JNodeTestCB", "testMethod", 3, 4);
-console.log("testMethod result (3+4): " + testMethodResult);
+var testMethodResult = JNodeCB.callMethod('JNodeTestCB', 'testMethod', 3, 4);
+console.log('testMethod result (3+4): ' + testMethodResult);
 
-JNodeCB.callMethod("JNodeTestCB", "testMethodWithCallback", function() {
-  console.log("Got empty callback from Java");
+JNodeCB.callMethod('JNodeTestCB', 'testMethodWithCallback', function() {
+  console.log('Got empty callback from Java');
 });
 
 console.log('END OF TEST');
